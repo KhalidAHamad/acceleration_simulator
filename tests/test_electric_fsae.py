0036a9be823228651eb7a2391726c73515138b05
Khalid Hamad <khalidazhmd@gmail.com>
2022-11-03 16:41:42 +0800

    calculate and plot acceleration

--- tests/test_electric_fsae.py
+++ tests/test_electric_fsae.py
@@ -1,8 +1,16 @@
 import csv
 import pytest
-
-
 from vehicle import Vehicle
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+# logger_formatter =
+logger_file_handler = logging.FileHandler("logs/test_vehicle.log")
+# logger_file_handler.setFormatter(logger_formatter)
+
+logger.addHandler(logger_file_handler)
+
 
 TEST_DATA_FILE = "data/standard_parameters.csv"
 
@@ -20,8 +28,28 @@ def test_init_from_file(ElectFSAE):
         p = {}
         reader = csv.reader(file)
         for row in reader:
-            p[row[0]] = row[1]
+            p[row[0]] = float(row[1])
 
-    acc = float(p["mue"]) * 9.807
+    g = ElectFSAE.G
+    assert g == 9.807, "Gravitational acceleration is not the expected value"
+    acc = p["mue"] * g
 
-    assert ElectFSAE.acceleration == pytest.approx(acc)
+    # v = 0, thus, F_drag and F_lift = 0
+    assert ElectFSAE.get_acceleration() == pytest.approx(acc)
+
+    # setting velocity equal to 1 and checking the calculations
+    v_test = 1
+    ElectFSAE.velocity = v_test
+    C_df = -p["lift_coefficient"]
+    C_d = p["drag_coefficient"]
+    A = p["frontal_area"]
+    M = p["mass"]
+    acc_w_velocity = p["mue"] * g + \
+    ((0.5 * 1.2 * A * (p["mue"] * C_df - C_d) * (v_test**2)) / M)
+    logger.debug(f"Value from Vehicle module = {ElectFSAE.get_acceleration()}"
+    f" Value from our calculations = {acc_w_velocity}")
+    logger.debug(f"{ElectFSAE.get_acceleration() is acc_w_velocity=}")
+
+    assert ElectFSAE.get_acceleration() == pytest.approx(
+        acc_w_velocity
+    ), "Acceleration is wrong when velocity is set"
--- vehicle.py
+++ vehicle.py
@@ -1,31 +1,45 @@
 import csv
+import logging
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+# logger_formatter =
+logger_file_handler = logging.FileHandler("logs/vehicle.log", mode="w")
+formatter = logging.Formatter(fmt='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger_file_handler.setFormatter(formatter)
+# logger_file_handler.setFormatter(logger_formatter)
+
+logger.addHandler(logger_file_handler)
 
 
 class Vehicle:
-    G = 9.807
+    G = 9.807  # Gravitational acceleration
+    RHO = 1.2  # Air density
     # rolling_coef = 0.98             # used to estimate tire's rolling radius
 
     def __init__(self, **kwargs):
-                # (self, mass, fric_coef, roll_radius, torque, gear_ratio, trans_eff,
-                # frontal_area, cd, cl, cg_height, wb_length, weight_dist, cp_location):
-        ## Parsing args from user
-        
         # Chassis and Tire Parameters
         self.mass = kwargs["mass"]
-        self.mue = kwargs["mue"]
-        self.weight = self.G * self.mass
-        # self._rolling_radius = self._parameters["static_tire_radius"] * self.rolling_coef
-        # Powertrain Parameters
-        # self._torque = self._parameters["engine_torque"]
-        # self._gear_ratio = self._parameters["gear_ratio"]
-        # self._trans_eff = self._parameters["transmission_eff"]
+        self.mue = kwargs["mue"]  # coefficient of friction
         # aero
-        
+        self.f_area = kwargs["frontal_area"]
+        self.drag_cof = kwargs["drag_coefficient"]
+        self.lift_cof = kwargs["lift_coefficient"]
+
         ## calculating args
-        self._acceleration = self.acceleration
+        self.weight = self.G * self.mass
+        self.velocity = 0
+        self.velocity_vec = [self.velocity]
+        self.acceleration_vec = []
+        self.position = 0
+        self.position_vec = [self.position]
+        self.time_vec = None
 
     @classmethod
-    def from_file(cls, file_name: str):
+    def from_file(cls, file_name: str = "data/standard_parameters.csv"):
         parameters = {}
         with open(file_name, encoding="utf-8") as file:
             reader = csv.reader(file)
@@ -33,14 +47,75 @@ class Vehicle:
                 value = row[1]
                 parameters[row[0]] = int(value) if value.isdigit() else float(value)
 
-        return cls(mass=parameters["mass"], mue=parameters["mue"])
+        return cls(**parameters)
 
-    @property
-    def acceleration(self):
-        frictional_forces = self.mue * self.weight
-        return frictional_forces / self.mass
+    def get_acceleration(self) -> float:
+        # F_normal = W + F_downforce
+        normal_force = self.weight - self.get_lift_force()
+        # F_friction = mue * F_normal
+        friction_force = self.mue * normal_force  # tractive frictional forces
+        # acceleration = sum_of_forces(F_friction - F_drag) / self.mass
+        acceleration = (friction_force - self.get_drag_force()) / self.mass
+        assert acceleration >= 0, "Acceleration should be either 0 or a positive number"
+        logger.info(f"{normal_force=} {friction_force=} {acceleration=}")
+        return acceleration
+
+    def get_lift_force(self) -> float:
+        lift = 0.5 * self.RHO * self.f_area * self.lift_cof * (self.velocity ** 2)
+        logger.debug(
+            "Lift force = "
+            f"{0.5} * {self.RHO=} * {self.f_area=} * {self.lift_cof=} * {self.velocity=} = {lift}"
+        )
+        assert lift <= 0, "Lift force is expected to be a negative number"
+        return lift
+
+    def get_drag_force(self) -> float:
+        drag = 0.5 * self.RHO * self.f_area * self.drag_cof * (self.velocity ** 2)
+        logger.debug(
+            "Drag force = "
+            f"{0.5} * {self.RHO=} * {self.f_area=} * {self.drag_cof=} * {self.velocity=} = {drag}"
+        )
+        assert drag >= 0, "Drag force is a negative number"
+        if drag == 0:
+            logger.warning(
+                "Drag force is equal to zero, "
+                f"{0.5=} * {self.RHO=} * {self.f_area=} * {self.drag_cof=} * {self.velocity=}"
+            )
+        return drag
+
+    def simulate_acceleration(self, ti=0, tf=5, h=0.001):
+        # range doesn't include tf=5, because we at each iteration, we calculate
+        # velocity and position at the next step
+        self.time_vec = np.arange(ti, tf + h, h)
+        assert self.time_vec[-1] == tf
+
+        for _ in range(len(self.time_vec) - 1):
+            acc = self.get_acceleration()
+            self.acceleration_vec.append(acc)
+            self.velocity = self.velocity + acc * h
+            self.velocity_vec.append(self.velocity * 3.6)
+            self.position = self.position + self.velocity * h
+            self.position_vec.append(self.position)
+        
+        self.acceleration_vec.append(self.get_acceleration())
+
+        # assert len(self.time_vec) == len(self.velocity_vec)
+        assert len(self.velocity_vec) == len(self.position_vec)
+        assert len(self.velocity_vec) == len(self.acceleration_vec)
+
+    def generate_plots(self):
+        fig, ax = plt.subplots()
+        ax.plot(self.time_vec, self.position_vec, color="b")
+        ax.plot(self.time_vec, self.velocity_vec, color="r")
+        plt.show()
 
 
 
-# if __name__ == "__main__":
-    # main()
+def main():
+    car = Vehicle.from_file()
+    car.simulate_acceleration(ti=0, tf=5, h=1)
+    car.generate_plots()
+
+
+if __name__ == "__main__":
+    main()
