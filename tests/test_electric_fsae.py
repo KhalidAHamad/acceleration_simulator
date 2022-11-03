60bc9e4964fec7d7efde3bdf5fd037d8a85b95c0
Khalid Hamad <khalidazhmd@gmail.com>
2022-11-03 11:39:12 +0800

    added 'data' folder to store vehicle parameters

--- tests/test_electric_fsae.py
+++ tests/test_electric_fsae.py
@@ -5,16 +5,18 @@ from pathlib import Path
 
 from vehicle import Vehicle
 
+TEST_DATA_FILE = "data/standard_parameters.csv"
+
 @pytest.fixture
 def ElectFSAE():
     # main_file_path = Path(__file__).parents[1]
     # csv_files = sorted(main_file_path.glob("*.csv"))
     # parameters_file = csv_files[0]
-    return Vehicle.from_file("parameters.csv")
+    return Vehicle.from_file(TEST_DATA_FILE)
 
 
-def test_init_from_files(ElectFSAE):
-    with open("parameters.csv") as file:
+def test_init_from_file(ElectFSAE):
+    with open(TEST_DATA_FILE) as file:
         p = {}
         reader = csv.reader(file)
         for row in reader:
